598bf78b9f643bc17321e176e2c47763d00bb758
master <wwtt1984@163.com>
2014-01-02 15:11:30 +0800

    first app

---
+++ www/vpn.js
@@ -0,0 +1,11 @@
+﻿var Vpn = function () {};
+/**
+ * 设置提示值
+ * @param user
+ * @param pwd
+ * @returns {*}
+ */
+Vpn.prototype.VpnLogin = function (user,pwd) {
+    return cordova.exec(null, null,"VpnPlugin","Vpn",[user,pwd]);
+};
+module.exports = (new Vpn());
